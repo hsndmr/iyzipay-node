bbc966c5b3743d794220d95a3bc4bbf2a86e1a75
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-25 00:12:37 +0300

    Add BasketItem class

---
+++ packages/iyzipay-node/src/lib/models/basket-item.ts
@@ -0,0 +1,105 @@
+import { BaseModel } from '../base-model';
+import { FlexibleData } from '../flexible-data.interface';
+import { RequestDataBuilder } from '../request-data-builder';
+import { RequestStringBuilder } from '../request-string-builder';
+
+export class BasketItem extends BaseModel {
+  private id: string;
+  private price: string;
+  private name: string;
+  private category1: string;
+  private category2: string;
+  private itemType: string;
+  private subMerchantKey: string;
+  private subMerchantPrice: string;
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setId(id: string): void {
+    this.id = id;
+  }
+
+  public getPrice(): string {
+    return this.price;
+  }
+
+  public setPrice(price: string): void {
+    this.price = price;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getCategory1(): string {
+    return this.category1;
+  }
+
+  public setCategory1(category1: string): void {
+    this.category1 = category1;
+  }
+
+  public getCategory2(): string {
+    return this.category2;
+  }
+
+  public setCategory2(category2: string): void {
+    this.category2 = category2;
+  }
+
+  public getItemType(): string {
+    return this.itemType;
+  }
+
+  public setItemType(itemType: string): void {
+    this.itemType = itemType;
+  }
+
+  public getSubMerchantKey(): string {
+    return this.subMerchantKey;
+  }
+
+  public setSubMerchantKey(subMerchantKey: string): void {
+    this.subMerchantKey = subMerchantKey;
+  }
+
+  public getSubMerchantPrice(): string {
+    return this.subMerchantPrice;
+  }
+
+  public setSubMerchantPrice(subMerchantPrice: string): void {
+    this.subMerchantPrice = subMerchantPrice;
+  }
+
+  public getJsonObject(): FlexibleData {
+    return RequestDataBuilder.create()
+      .add('id', this.getId())
+      .add('price', this.getPrice())
+      .add('name', this.getName())
+      .add('category1', this.getCategory1())
+      .add('category2', this.getCategory2())
+      .add('itemType', this.getItemType())
+      .add('subMerchantKey', this.getSubMerchantKey())
+      .add('subMerchantPrice', this.getSubMerchantPrice())
+      .get();
+  }
+
+  public toPKIRequestString(): string {
+    return RequestStringBuilder.create()
+      .append('id', this.getId())
+      .append('price', this.getPrice())
+      .append('name', this.getName())
+      .append('category1', this.getCategory1())
+      .append('category2', this.getCategory2())
+      .append('itemType', this.getItemType())
+      .append('subMerchantKey', this.getSubMerchantKey())
+      .append('subMerchantPrice', this.getSubMerchantPrice())
+      .getRequestString();
+  }
+}
