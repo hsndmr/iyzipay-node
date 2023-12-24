dad875a007dfd355b8b16c17c39c9f01a9354260
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-24 21:23:51 +0300

    Add tests for handling arrays in RequestStringBuilder

--- packages/iyzipay-node/src/lib/request-string-builder.spec.ts
+++ packages/iyzipay-node/src/lib/request-string-builder.spec.ts
@@ -34,4 +34,61 @@ describe('RequestStringBuilder', () => {
     // Act & Assert
     expect(builder.getRequestString()).toEqual('[key=mockValue]');
   });
+
+  it('should handle an empty array', () => {
+    // Arrange
+    const builder = RequestStringBuilder.create();
+    builder.appendArray('testKey', []);
+
+    // Act & Assert
+    expect(builder.getRequestString()).toEqual('[testKey=[]]');
+  });
+
+  it('should append an array of strings', () => {
+    // Arrange
+    const builder = RequestStringBuilder.create();
+    builder.appendArray('testKey', ['value1', 'value2']);
+
+    // Act & Assert
+    expect(builder.getRequestString()).toEqual('[testKey=[value1, value2]]');
+  });
+
+  it('should ignore null values in the array', () => {
+    // Arrange
+    const builder = RequestStringBuilder.create();
+    builder.appendArray('testKey', ['value1', null, 'value2']);
+
+    // Act & Assert
+    expect(builder.getRequestString()).toEqual('[testKey=[value1, value2]]');
+  });
+
+  it('should append an array of RequestStringConvertible objects', () => {
+    // Arrange
+    const convertible1 = {
+      toPKIRequestString: () => 'value1PKI',
+    };
+
+    const convertible2 = {
+      toPKIRequestString: () => 'value2PKI',
+    };
+    const builder = RequestStringBuilder.create();
+    builder.appendArray('testKey', [convertible1, convertible2]);
+
+    // Act & Assert
+    expect(builder.getRequestString()).toEqual(
+      '[testKey=[value1PKI, value2PKI]]'
+    );
+  });
+
+  it('should handle a mixed array', () => {
+    // Arrange
+    const convertible = {
+      toPKIRequestString: () => 'value1PKI',
+    };
+    const builder = RequestStringBuilder.create();
+    builder.appendArray('testKey', [convertible, 'value2']);
+
+    // Act & Assert
+    expect(builder.getRequestString()).toEqual('[testKey=[value1PKI, value2]]');
+  });
 });
