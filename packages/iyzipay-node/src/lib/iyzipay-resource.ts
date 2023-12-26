c17e069c8674ade8125ae1bcfef9b3d17000fca0
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-27 01:50:26 +0300

    Add IyzipayResource class to iyzipay-node package

---
+++ packages/iyzipay-node/src/lib/iyzipay-resource.ts
@@ -0,0 +1,67 @@
+import { ApiResource } from './api-resource';
+
+class IyzipayResource extends ApiResource {
+  private status: string;
+  private errorCode: string;
+  private errorMessage: string;
+  private errorGroup: string;
+  private locale: string;
+  private systemTime: number;
+  private conversationId: string;
+
+  public getStatus(): string {
+    return this.status;
+  }
+
+  public setStatus(status: string): void {
+    this.status = status;
+  }
+
+  public getErrorCode(): string {
+    return this.errorCode;
+  }
+
+  public setErrorCode(errorCode: string): void {
+    this.errorCode = errorCode;
+  }
+
+  public getErrorMessage(): string {
+    return this.errorMessage;
+  }
+
+  public setErrorMessage(errorMessage: string): void {
+    this.errorMessage = errorMessage;
+  }
+
+  public getErrorGroup(): string {
+    return this.errorGroup;
+  }
+
+  public setErrorGroup(errorGroup: string): void {
+    this.errorGroup = errorGroup;
+  }
+
+  public getLocale(): string {
+    return this.locale;
+  }
+
+  public setLocale(locale: string): void {
+    this.locale = locale;
+  }
+
+  public getSystemTime(): number {
+    return this.systemTime;
+  }
+
+  public setSystemTime(systemTime: number): void {
+    this.systemTime = systemTime;
+  }
+
+  public getConversationId(): string {
+    return this.conversationId;
+  }
+
+  public setConversationId(conversationId: string): void {
+    this.conversationId = conversationId;
+  }
+}
