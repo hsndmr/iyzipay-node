d8fa3939484899947a61ed64cea65ef5d958c308
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-28 00:19:40 +0300

    Refactor IyzipayResource class to use optional properties

--- packages/iyzipay-node/src/lib/models/resources/iyzipay-resource.ts
+++ packages/iyzipay-node/src/lib/models/resources/iyzipay-resource.ts
@@ -1,67 +1,67 @@
 import { ApiResource } from '../../api-resource';
 
 export class IyzipayResource extends ApiResource {
-  private status: string;
-  private errorCode: string;
-  private errorMessage: string;
-  private errorGroup: string;
-  private locale: string;
-  private systemTime: number;
-  private conversationId: string;
+  private status?: string;
+  private errorCode?: string;
+  private errorMessage?: string;
+  private errorGroup?: string;
+  private locale?: string;
+  private systemTime?: number;
+  private conversationId?: string;
 
-  public getStatus(): string {
+  public getStatus(): string | undefined {
     return this.status;
   }
 
-  public setStatus(status: string): void {
+  public setStatus(status?: string): void {
     this.status = status;
   }
 
-  public getErrorCode(): string {
+  public getErrorCode(): string | undefined {
     return this.errorCode;
   }
 
-  public setErrorCode(errorCode: string): void {
+  public setErrorCode(errorCode?: string): void {
     this.errorCode = errorCode;
   }
 
-  public getErrorMessage(): string {
+  public getErrorMessage(): string | undefined {
     return this.errorMessage;
   }
 
-  public setErrorMessage(errorMessage: string): void {
+  public setErrorMessage(errorMessage?: string): void {
     this.errorMessage = errorMessage;
   }
 
-  public getErrorGroup(): string {
+  public getErrorGroup(): string | undefined {
     return this.errorGroup;
   }
 
-  public setErrorGroup(errorGroup: string): void {
+  public setErrorGroup(errorGroup?: string): void {
     this.errorGroup = errorGroup;
   }
 
-  public getLocale(): string {
+  public getLocale(): string | undefined {
     return this.locale;
   }
 
-  public setLocale(locale: string): void {
+  public setLocale(locale?: string): void {
     this.locale = locale;
   }
 
-  public getSystemTime(): number {
+  public getSystemTime(): number | undefined {
     return this.systemTime;
   }
 
-  public setSystemTime(systemTime: number): void {
+  public setSystemTime(systemTime?: number): void {
     this.systemTime = systemTime;
   }
 
-  public getConversationId(): string {
+  public getConversationId(): string | undefined {
     return this.conversationId;
   }
 
-  public setConversationId(conversationId: string): void {
+  public setConversationId(conversationId?: string): void {
     this.conversationId = conversationId;
   }
 }
