ed11bbc23c0ce0f430475c825c144d4204183c51
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-29 00:10:16 +0300

    Refactor create-payment route in main.ts

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -4,11 +4,12 @@ import { createPayment } from './samples/create-payment';
 const app = express();
 
 app.get('/create-payment', (req, res) => {
-  createPayment();
-
-  res.send('OK');
+  createPayment()
+    .then((result) => {})
+    .catch((err) => {})
+    .finally(() => {
+      res.send('OK');
+    });
 });
 
-app.listen(3000, () => {
-  console.log('app listening on port 3000!');
-});
+app.listen(3000, () => {});
