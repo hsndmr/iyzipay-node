dc39991c29f102fc207d006f3726057f5a2bfe50
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-28 22:34:47 +0300

    Refactor PaymentResourceMapper to use ResponseDataManager

--- packages/iyzipay-node/src/lib/models/mappers/payment-resource-mapper.ts
+++ packages/iyzipay-node/src/lib/models/mappers/payment-resource-mapper.ts
@@ -1,12 +1,50 @@
-import { PaymentResource } from '../resources/payment-resource';
 import { IyzipayResourceMapper } from './iyzipay-resource-mapper';
-
+import { ResponseDataManager } from '../../response-data-manager';
+import { PaymentResource } from '../resources/payment-resource';
+import { PaymentItemMapper } from './payment-item-mapper';
 export class PaymentResourceMapper extends IyzipayResourceMapper {
   public mapResourceFrom(
     resource: PaymentResource,
     responseData
   ): PaymentResource {
     super.mapResourceFrom(resource, responseData);
+
+    const responseDataManager = new ResponseDataManager(responseData);
+
+    resource.setPrice(responseDataManager.get('price'));
+    resource.setPaidPrice(responseDataManager.get('paidPrice'));
+    resource.setInstallment(responseDataManager.get('installment'));
+    resource.setPaymentId(responseDataManager.get('paymentId'));
+    resource.setPaymentStatus(responseDataManager.get('paymentStatus'));
+    resource.setFraudStatus(responseDataManager.get('fraudStatus'));
+    resource.setMerchantCommissionRate(
+      responseDataManager.get('merchantCommissionRate')
+    );
+    resource.setMerchantCommissionRateAmount(
+      responseDataManager.get('merchantCommissionRateAmount')
+    );
+    resource.setIyziCommissionRateAmount(
+      responseDataManager.get('iyziCommissionRateAmount')
+    );
+    resource.setIyziCommissionFee(responseDataManager.get('iyziCommissionFee'));
+    resource.setCardType(responseDataManager.get('cardType'));
+    resource.setCardAssociation(responseDataManager.get('cardAssociation'));
+    resource.setCardFamily(responseDataManager.get('cardFamily'));
+    resource.setCardUserKey(responseDataManager.get('cardUserKey'));
+    resource.setCardToken(responseDataManager.get('cardToken'));
+    resource.setBinNumber(responseDataManager.get('binNumber'));
+    resource.setBasketId(responseDataManager.get('basketId'));
+    resource.setCurrency(responseDataManager.get('currency'));
+    resource.setPaymentItems(
+      new PaymentItemMapper().mapPaymentItems(
+        responseDataManager.get('itemTransactions')
+      )
+    );
+    resource.setConnectorName(responseDataManager.get('connectorName'));
+    resource.setAuthCode(responseDataManager.get('authCode'));
+    resource.setPhase(responseDataManager.get('phase'));
+    resource.setLastFourDigits(responseDataManager.get('lastFourDigits'));
+    resource.setPosOrderId(responseDataManager.get('posOrderId'));
 
     return resource;
   }
