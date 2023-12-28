087786893d112d9d64fdd5551cd2898b118b0467
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-28 22:33:14 +0300

    Refactor IyzipayResourceMapper to use setResponseData method

--- packages/iyzipay-node/src/lib/models/mappers/iyzipay-resource-mapper.ts
+++ packages/iyzipay-node/src/lib/models/mappers/iyzipay-resource-mapper.ts
@@ -3,30 +3,34 @@ import { IyzipayResource } from '../resources/iyzipay-resource';
 
 export class IyzipayResourceMapper {
   protected responseData: ResponseData;
-  protected responseDataManager: ResponseDataManager;
 
   constructor(data: ResponseData) {
+    this.setResponseData(data);
+  }
+
+  setResponseData(data: ResponseData) {
     this.responseData = data;
-    this.responseDataManager = new ResponseDataManager(data);
   }
 
   public mapResourceFrom(
     resource: IyzipayResource,
-    responseData: ResponseData
-  ) {
-    resource.setStatus(this.responseDataManager.get('status'));
-    resource.setConversationId(this.responseDataManager.get('conversationId'));
-    resource.setErrorCode(this.responseDataManager.get('errorCode'));
-    resource.setErrorMessage(this.responseDataManager.get('errorMessage'));
-    resource.setErrorGroup(this.responseDataManager.get('errorGroup'));
-    resource.setLocale(this.responseDataManager.get('locale'));
-    resource.setSystemTime(this.responseDataManager.get('systemTime'));
-    resource.setRawResult(this.responseDataManager.get('rawResult'));
+    responseData
+  ): IyzipayResource {
+    const responseDataManager = new ResponseDataManager(responseData);
+
+    resource.setStatus(responseDataManager.get('status'));
+    resource.setConversationId(responseDataManager.get('conversationId'));
+    resource.setErrorCode(responseDataManager.get('errorCode'));
+    resource.setErrorMessage(responseDataManager.get('errorMessage'));
+    resource.setErrorGroup(responseDataManager.get('errorGroup'));
+    resource.setLocale(responseDataManager.get('locale'));
+    resource.setSystemTime(responseDataManager.get('systemTime'));
+    resource.setRawResult(responseData);
 
     return resource;
   }
 
-  public mapResource(resource: IyzipayResource) {
+  public mapResource(resource: IyzipayResource): IyzipayResource {
     return this.mapResourceFrom(resource, this.responseData);
   }
 }
