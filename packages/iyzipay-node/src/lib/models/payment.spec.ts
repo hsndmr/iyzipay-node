ed681013d907532bda7e6cef4c485da63be84c58
demirjs <41634504+hsndmr@users.noreply.github.com>
2023-12-30 20:52:45 +0300

    Add payment creation test case

---
+++ packages/iyzipay-node/src/lib/models/payment.spec.ts
@@ -0,0 +1,24 @@
+import { IyzipayHttpClient } from '../iyzipay-http-client';
+import { Options } from '../options';
+import { CreatePaymentRequest } from '../requests';
+import {
+  IyzipayHttpClientMock,
+  assertResource,
+} from '../iyzipay-http-client.mock';
+import { Payment } from './payment';
+
+describe('Payment', () => {
+  it('should create payment', async () => {
+    // Arrange
+    const options = new Options();
+    const client = new IyzipayHttpClientMock(options);
+
+    const request = new CreatePaymentRequest();
+
+    // Act
+    const payment = await Payment.create(request, client);
+
+    // Assert
+    assertResource(payment);
+  });
+});
